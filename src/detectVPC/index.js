3fe231e2ad3c2a11f0ac248b91d9da8d9a69221a
Anna Spysz <anna.spysz@stackery.io>
2019-06-24 16:55:57 -0700

    Added function code

--- src/detectVPC/index.js
+++ src/detectVPC/index.js
@@ -1,6 +1,123 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const aws = require('aws-sdk');
 
-  return {};
+// TODO: Make it work for all regions!
+
+exports.handler = async () => {
+  try {
+    console.log(`Beginning VPC detection`);
+    await findResources();
+  } catch (error) {
+    console.log(error);
+  }
+
+  // return a 200 response if no errors
+  const response = {
+    statusCode: 200,
+    body: `Done.`
+  };
+
+  return response;
 };
+
+async function findResources (resource) {
+  const cs = new aws.ConfigService();
+
+  const params = {
+    ConfigurationAggregatorName: 'vpc-eradicator', /* required */
+    ResourceType: 'AWS::EC2::VPC'
+  };
+
+  // Use AWS Config to find all VPCs
+  const resources = await cs.listAggregateDiscoveredResources(params).promise();
+  console.log(resources);
+
+  if (resources.ResourceIdentifiers.length > 0) { // run eradicateResources function only if the resource was found
+    let count = resources.ResourceIdentifiers.length === 1 ? 'VPC' : 'VPCs'; // good grammar is important
+    console.log(`Oh noes! ${resources.ResourceIdentifiers.length} ${count} discovered! Sending warning email.`);
+    // Run eradicateResources function on each instance of that resource
+    await sendWarningEmail(resources);
+  } else {
+    console.log(`No VPCs found here, your money is safe for now!`);
+  }
+}
+
+// send email
+async function sendWarningEmail(resources) {
+  try {
+    // generate email
+    body = await generateEmailBody(resources);
+    header = `VPC Warning`;
+    // send the email
+    console.log(`Sending email.`);
+    await sendEmail(header, body);
+    console.log(`Email sent`);
+  } catch (error) {
+    console.log(`Error sending email`);
+    console.log(error);
+  }
+}
+
+// make a pretty email body
+function generateEmailBody (resources) {
+  const header = `<h1>⚠️ VPC Warning ⚠️</h1>`;
+  const summary = `
+    <div>#️⃣ Number of VPCs found: ${resources.ResourceIdentifiers.length}</div>
+  `;
+
+  let instances = '';
+  resources.ResourceIdentifiers.forEach(resource => {
+    const item = `<li>${resource.ResourceId}</li>`;
+    instances = instances + item;
+  });
+
+  const style = `
+    <style>
+      h1, h2, h3, div {font-family: sans-serif;}
+    </style>
+    `;
+
+  return `
+    <html>
+      <head>${style}</head>
+      <body>
+        ${header}
+        <div>
+          ${summary}
+        </div>
+        <br/><hr/><br/>
+        <div>
+          <h3>Instances found:</h3>
+          <ul>
+            ${instances}
+          </ul>
+        </div>
+      </body>
+    </html>
+  `;
+};
+
+async function sendEmail (subject, body) {
+  let params = {
+    Destination: {
+      ToAddresses: [
+        'anna.spysz@stackery.io'
+      ]
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: body
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: subject
+      }
+    },
+    Source: 'anna.spysz@stackery.io'
+  };
+
+  const ses = new AWS.SES();
+  await ses.sendEmail(params).promise();
+}
